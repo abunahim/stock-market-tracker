7a6eeefc0f054334d53bd2f61d4e9c0f68d6fa98
abunahim <abunahim5@gmail.com>
2026-05-14 03:15:21 +0100

    Initial project setup

---
+++ backend/stock-market-tracker/src/main/java/com/stock_market_tracker/stock_market_tracker/StockMarketTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stock_market_tracker.stock_market_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketTrackerApplication.class, args);
+	}
+
+}
